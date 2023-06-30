399970a9f2d9bcf1c1edd2ed8c672c46c8cd7d46
Redjorm <redjorm12@gmail.com>
2023-06-30 10:46:56 -0400

    V4

--- src/models/index.js
+++ src/models/index.js
@@ -1,16 +1,20 @@
-const Product = require("./Product")
-const Category = require("./Category")
-const Cart = require("./Cart")
-const User = require("./User")
+const Product = require('./Product')
+const Category = require('./Category')
+const Cart = require('./Cart')
+const User = require('./User')
+const Purchase = require('./Purchase')
 
-//Product -> //categoryId
 Product.belongsTo(Category)
 Category.hasMany(Product)
 
-//Cart -> //userId
 Cart.belongsTo(User)
 User.hasOne(Cart)
 
-//Cart -> //productId
 Cart.belongsTo(Product)
 Product.hasMany(Cart)
+
+Purchase.belongsTo(User)
+User.hasMany(Purchase)
+
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
