955ad582e034feb8c783ccf3397a3af78e0c6f13
Redjorm <redjorm12@gmail.com>
2023-07-04 14:22:40 -0400

    V6

--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -2,12 +2,14 @@ const request = require('supertest')
 const app = require('../app')
 require('../models')
 const Category = require('../models/Category')
+const ProductImg = require('../models/ProductImg')
 ////////////////////////////
 const BASE_URL_USERS = '/api/v1/users/login'
 let TOKEN
 const BASE_URL_PRODUCTS = '/api/v1/products'
 let category
 let productId
+let productImg
 
 beforeAll(async () => {
   const user = {
@@ -51,6 +53,7 @@ test("GET -> 'BASE_URL_PRODUCTS' should status code 200, res.body.length === 1 a
   expect(res.status).toBe(200)
   expect(res.body).toHaveLength(1)
   expect(res.body[0].category).toBeDefined()
+  expect(res.body[0].productImgs).toBeDefined()
 })
 
 test("GET -> 'BASE_URL_PRODUCTS?category = category.id' should status code 200, res.body.length === 1 and res.body[0] to be defined", async () => {
@@ -61,6 +64,7 @@ test("GET -> 'BASE_URL_PRODUCTS?category = category.id' should status code 200, 
   expect(res.status).toBe(200)
   expect(res.body).toHaveLength(1)
   expect(res.body[0].category).toBeDefined()
+  expect(res.body[0].productImgs).toBeDefined()
 })
 
 test("GET ONE -> 'BASE_URL_PRODUCTS/:id' should status code 200 and res.body.title === xiaomi 12", async () => {
@@ -69,6 +73,7 @@ test("GET ONE -> 'BASE_URL_PRODUCTS/:id' should status code 200 and res.body.tit
   expect(res.status).toBe(200)
   expect(res.body.title).toBe('xiaomi 12')
   expect(res.body.category).toBeDefined()
+  expect(res.body.productImgs).toBeDefined()
 })
 
 test("PUT -> 'BASE_URL_PRODUCTS/:id' should status code 200 and res.body.title === body.title", async () => {
@@ -85,6 +90,24 @@ test("PUT -> 'BASE_URL_PRODUCTS/:id' should status code 200 and res.body.title =
   expect(res.body.title).toBe(product.title)
 })
 
+test("POST -> 'BASE_URL_PRODUCTS/:id/images', should return status code 200, and res.body.length === 1", async () => {
+  const productImgBody = {
+    url: 'http://localhost:8080/api/v1/public/uploads/cocina.jpg',
+    filename: 'cocina.jpg',
+    productId
+  }
+
+  productImg = await ProductImg.create(productImgBody)
+
+  const res = await request(app)
+    .post(`${BASE_URL_PRODUCTS}/${productId}/images`)
+    .send([productImg.id])
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+  expect(res.status).toBe(200)
+  expect(res.body).toHaveLength(1)
+})
+
 test("DELETE -> 'BASE_URL_PRODUCTS/:id' should status code 204", async () => {
   const res = await request(app)
     .delete(`${BASE_URL_PRODUCTS}/${productId}`)
@@ -93,4 +116,5 @@ test("DELETE -> 'BASE_URL_PRODUCTS/:id' should status code 204", async () => {
   expect(res.status).toBe(204)
 
   await category.destroy()
+  await productImg.destroy()
 })
